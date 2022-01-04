d1cf26c1bb986f0f4aee2eb85f583c2e677c05d0
Sebastien Colbert <colbertsebastien@gmail.com>
2022-01-04 07:24:02 +0100

    Add test API par Role

---
+++ villagealert/__tests__/api/api_spec_admin.js
@@ -0,0 +1,180 @@
+const frisby = require('frisby')
+const Joi = frisby.Joi
+const baseUrl = 'http://localhost:8000'
+// const baseUrl = 'https://villagealert.herokuapp.com'
+const timestamp = Date.now()
+const emailTest = `testAPI${timestamp}@gmail.com`
+const passwordTest = 'mdp123'
+let authToken
+
+//
+// Citoyen
+//
+
+// Test de création de compte
+it('Should be signup', function () {
+  return frisby.post(baseUrl + '/api/users/signup', {
+    email: emailTest,
+    password: passwordTest,
+    firstname: 'Pat',
+    lastname: 'Hibulaire',
+    address: '42 rue nullepart ailleurs',
+    phone: '0123456789',
+    city: 'Calais',
+    postCode: '62200'
+  })
+    .expect('status', 200)
+})
+
+// Test de connexion
+it('Should be login', function () {
+  return frisby.post(baseUrl + '/api/users/login', {
+    email: emailTest,
+    password: passwordTest
+  })
+    .expect('status', 200)
+    .expect('jsonTypes', {
+      token: Joi.string().required()
+    })
+    .then(function (res) {
+      const data = JSON.parse(res.body)
+      authToken = Buffer.from(data.token)
+    })
+})
+
+//
+// Citoyen connecté
+//
+
+// Test get account
+it('Should have this info user', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .get(baseUrl + '/api/users/account')
+    .expect('status', 200)
+})
+
+// Test patch account
+it('Should update user account', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .patch(baseUrl + '/api/users/account', {
+      firstname: 'Luke',
+      lastname: 'Skywalker'
+    })
+    .expect('status', 200)
+    .expect('json', { message: 'Votre compte a été modifié !' })
+})
+
+// Test delete account
+it('Should delete user account', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .delete(baseUrl + '/api/users/account')
+    .expect('status', 200)
+    .expect('json', { message: 'Votre compte a été supprimé !' })
+})
+
+// Test post alert
+it('Should post my Alert', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .post(baseUrl + '/api/alerts/', {
+      typeAlert: 'voirie',
+      description: 'Mon voisin est un con finis !',
+      addressAlert: 'Mars'
+    })
+    .expect('status', 201)
+    .expect('json', { message: 'Votre alerte a bien été enregistrée!' })
+})
+
+// Test get alert
+it('Should get list Alert', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .get(baseUrl + '/api/alerts/')
+    .expect('status', 200)
+})
+
+// Test get alert id
+const idAlert = '61d2bd0602ff2b76a35750e9'
+it('Should get Alert id', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .get(baseUrl + '/api/alerts/' + idAlert)
+    .expect('status', 200)
+})
+
+// Test delete alert id
+it('Should delete Alert id', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .delete(baseUrl + '/api/alerts/' + idAlert)
+    .expect('status', 200)
+})
+
+//
+// Responsable
+//
+
+// Test patch alert id by responsable only
+const idAlert2 = '61d2bfeb0999c004c5717391'
+it('Should patch Alert id', function () {
+  return frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: 'Bearer ' + authToken
+        }
+      }
+    })
+    .patch(baseUrl + '/api/alerts/' + idAlert2,
+      {
+        firstname: 'Donald',
+        lastname: 'Duck'
+      }
+    )
+    .expect('status', 200)
+})
